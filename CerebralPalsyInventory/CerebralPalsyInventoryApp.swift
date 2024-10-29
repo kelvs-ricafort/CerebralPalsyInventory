f2cefa89ddf1b9ad37e1203f9519cd079306fd8f
Kelvin Ricafort <kelvs.r@outlook.com>
2024-10-29 16:09:33 +0800

    Initial Commit

---
+++ CerebralPalsyInventory/CerebralPalsyInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerebralPalsyInventoryApp.swift
+//  CerebralPalsyInventory
+//
+//  Created by Kelvin on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CerebralPalsyInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
